37f2856710e20fc9709c15671d9d8b05aa0ab2dd
wanderfulife <john.wander@hotmail.fr>
2022-02-27 13:54:10 +1100

    deployed rinkeby

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,7 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+const { API_URL, PRIVATE_KEY } = process.env;
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -16,13 +9,16 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
  module.exports = {
-  solidity: "0.8.4",
+  defaultNetwork: "hardhat",
   paths: {
     artifacts: './src/artifacts',
   },
   networks: {
-    hardhat: {
-      chainId: 1337
+    hardhat: {},
+    rinkeby: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY]
     }
-  }
+  },
+  solidity: "0.8.4",
 };
--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { ethers } from 'ethers'
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json'
 
 // Update with the contract address logged out to the CLI when it was deployed 
-const greeterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+const greeterAddress = "0x07b244EfDACf37D353cb0c688bab360aA84324f3"
 
 function App() {
   // store greeting in local state
@@ -22,7 +22,7 @@ function App() {
       const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
       try {
         const data = await contract.greet()
-        console.log('data: ', data)
+        console.log(data)
       } catch (err) {
         console.log("Error: ", err)
       }
